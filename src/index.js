5c315d225db6f26c1b11ecdb7c16123cdda65741
Sarath-KrishnanBiz <sarath.krishnan@bizcloudexperts.com>
2022-09-29 21:05:46 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import Navigation from './Navigation';
-import Test from './components/Horizontalbar';
+import Test from './components/Normallist';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
